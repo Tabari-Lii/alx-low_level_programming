6e08e8a8eb6749083c39a56210237038fb497d6d
Tabari-Lii <tabarishadow@gmail.com>
2022-10-15 08:59:57 -0700

    all

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main-Serves as the entry point 
+ * main-Serves as the entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
