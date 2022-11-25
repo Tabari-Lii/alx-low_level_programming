4b51260ef52677d8fb8de93215ffdddf6e0ca8d7
Tabari-Lii <tabarishadow@gmail.com>
2022-11-25 02:27:37 -0800

    0-print_list.c added

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,19 @@
  */
 size_t print_list(const list_t *h)
 {
-		size_t nodes = 0;
+	size_t nodes = 0;
 
-			while (h)
-					{
-								if (h->str == NULL)
-												printf("[0] (nil)\n");
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
-										else
-														printf("[%d] %s\n", h->len, h->str);
-												nodes++;
-														h = h->next;
-															}
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-				return (nodes);
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
 }
